221cbdfb331d6c36da9a2df8b40b025534f500df
Ben Drawer <bendrawer1@gmail.com>
2019-03-04 12:39:02 +0000

    Solution working

---
+++ index.js
@@ -0,0 +1,51 @@
+//file reader
+const reader = require('./reader');
+
+//room dimensions
+var dimensions = reader[0];
+//coordinates[0] represents the x-axis; coordinates[1] represents the y-axis
+var coordinates = [parseInt(reader[1][0]), parseInt(reader[1][1])];
+//directions
+var directions = reader[reader.length-1];
+//coordinates of dirty spots
+var dirt = reader.slice(2, reader.length-1);
+//counter for number of dirty spots cleaned
+var cleaned = 0;
+
+//Loop through each direction - 'N', 'S', 'E', 'W'
+directions.forEach((d) => {
+    switch(d) {
+        case 'N':
+            coordinates[1] += 1;
+            break;
+        case 'S':
+            coordinates[1] -= 1;
+            break;
+        case 'E':
+            coordinates[0] += 1;
+            break;
+        case 'W':
+            coordinates[0] -= 1;
+            break;
+        default:
+            break;
+    }
+
+    //Check that the hoover has not exceeded the dimensions
+    coordinates[0] >= dimensions[0] ? coordinates[0] = dimensions[0] - 1 : coordinates[0];
+    coordinates[1] >= dimensions[1] ? coordinates[1] = dimensions[1] - 1 : coordinates[1];
+
+    //Loop through coordinates of dirty spots
+    for (var i = 0; i < dirt.length; i++) {
+        //Check if current location needs to be cleaned
+        if (parseInt(dirt[i][0]) === coordinates[0] && parseInt(dirt[i][1]) === coordinates[1]) {
+            cleaned += 1;
+
+            //Replace coordinates of dirt with '-1' indicates that spot has now been cleaned
+            dirt[i] = -1;
+        }
+    }
+});
+
+console.log(coordinates[0] + ' ' + coordinates[1]);
+console.log(cleaned);
---
+++ reader.js
@@ -0,0 +1,19 @@
+/*
+This is the reader file.
+It takes a text file - input.txt, placed in the same directory - and
+converts it to an array.
+
+All but the last line of the text file should be two integers separated
+by a space, representing coordinates.
+The last line should be a string of directions, e.g., 'NNESW', which will
+be broken apart into individual characters.
+*/
+const fs = require('fs');
+
+var text = fs.readFileSync('./input.txt').toString();
+var data = [];
+
+text.split('\r\n').map((t) => data.push(t.split(' ')));
+data[data.length-1] = data[data.length-1][0].split('');
+
+module.exports = data;
